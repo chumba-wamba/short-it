48c956cc4b46a3a2785cd8a4b6fbf3c29cec87e0
chumba-wamba <nisarg0271@gmail.com>
2020-06-16 23:07:41 +0530

    Initialised Flask-MongoEngine

--- short_it/__init__.py
+++ short_it/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+
+
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {
+    'db': 'shortit',
+    'host': 'mongodb://localhost/shorit'
+}
+db = MongoEngine(app)
+
 
 from short_it import routes
